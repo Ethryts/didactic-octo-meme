7d9ff512cbac16f97279ce882643900bec9aa77e
Ethan <EthanHrytsak@gmail.com>
2022-06-30 21:40:40 -0600

    Set up vue, and bootstrap, and icons, and stuff, made some progress

---
+++ src/components/NavBar.vue
@@ -0,0 +1,25 @@
+<template>
+	<nav class="navbar navbar-expand fixed-top bg-light">
+		<div class="container-fluid">
+			<a class="navbar-brand" href="#">Ethan Hrytsak</a>
+			<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="navabrText"
+				aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+
+			<div class="collapse navbar-collapse" id="navbarText">
+
+				<ul class="navbar-nav me-auto mb-2 mb-lg-0">
+				<!--todo Use the vue to make the current selection active on the nav links--->
+					<li class="nav-item"> <a class="nav-link active" href="#">Home</a></li>
+					<li class="nav-item"> <a class="nav-link active" href="#">Tech</a></li>
+				</ul>
+			</div>
+
+		</div>
+	</nav>
+</template>
+
+
+<style scoped>
+</style>
